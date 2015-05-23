54c637358da209a3c3bf6db0d84ecb7675846d26
hellosputnik <hallosputnik@gmail.com>
2015-05-23 14:03:48 -0700

    Refactor ACM ICPC Team challenge
    I rewrote the code so that it is more procedural and less imperative. Some
    variable names have been changed to be a little more descriptive. It still
    might be confusing without a readme file. That will come in the future.

--- Algorithms/Warmup/ACM ICPC Team/acm_icpc_team.py
+++ Algorithms/Warmup/ACM ICPC Team/acm_icpc_team.py
@@ -1,17 +1,26 @@
-N, M = map(int, raw_input().split())
+def calculate(N, people):
+    teams, topics = 0, 0
 
-persons, teams, topics = [], 0, 0
+    for left in xrange(N):
+        for right in xrange(left + 1, N):
+            combo = str(bin(int(people[left], 2) | int(people[right], 2))).count("1")
+            if combo > topics:
+                teams, topics = 1, combo
+            elif combo == topics:
+                teams += 1
 
-for n in xrange(N):
-    persons.append(raw_input())
+    return ("%s\n%s" % (topics, teams))
+ 
+def main():
+    N, M = map(int, raw_input().split())
+    
+    people = []
+    
+    for n in xrange(N):
+        people.append(raw_input())
+    
+    print calculate(N, people)
 
-for one in xrange(N):
-    for two in xrange(one + 1, N):
-        y = str(bin(int(persons[one], 2) | int(persons[two], 2))).count("1")
-        if y > topics:
-            teams, topics = 1, y
-        elif y == topics:
-            teams += 1
-
-print "%s\n%s" % (topics, teams)
+if __name__ == '__main__':
+    main()
 
