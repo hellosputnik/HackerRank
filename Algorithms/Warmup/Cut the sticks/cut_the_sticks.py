1d58419ed9095808c8c2bd6b9eea38955a548c49
hellosputnik <hallosputnik@gmail.com>
2015-08-14 13:37:26 -0700

    Update categories of problems
    HackerRank recently changed the categories for some of the problems. I
    updated the repository to match the listing on the HackerRank site.

--- Algorithms/Warmup/Cut the sticks/cut_the_sticks.py
+++
@@ -1,9 +0,0 @@
-N = input()
-a = map(int, raw_input().split())
-
-while a:
-    print len(a)
-    smallest = min(a)
-    while smallest in a:
-        a.remove(smallest)
-
--- Algorithms/Warmup/Sherlock and Squares/sherlock_and_squares.py
+++
@@ -1,8 +0,0 @@
-T = input()
-
-for t in xrange(T):
-    A, B = map(int, raw_input().split())
-    squares = int(B ** 0.5) - int(A ** 0.5)
-    squares += 1 if int(A ** 0.5) ** 2 == A else 0
-    print squares
-
