70f330da5823f85467d7ad1f448ad0085be05c0d
hellosputnik <hallosputnik@gmail.com>
2016-10-21 15:16:00 -0700

    Add Regex domain

---
+++ Regex/Repetitions/Matching {x, y} Repetitions/matching_x_y_repititions.py
@@ -0,0 +1,3 @@
+# HackerRank misspelled "repetitions" in the filename. I sent an edit
+# suggestion, but I have no idea if the staff reads those.
+Regex_Pattern = r"^[\d]{1,2}[a-zA-Z]{3,}(\.){0,3}$"
